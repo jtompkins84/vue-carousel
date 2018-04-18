2dd38ece4c71dff582a44d11040ade29cb159c6b
joseph.tompkins <joseph.tompkins@gmail.com>
2018-04-18 11:51:37 -0500

    fix(source): change navigation.vue style bind format

--- src/Navigation.vue
+++ src/Navigation.vue
@@ -4,19 +4,15 @@
       class="VueCarousel-navigation-button VueCarousel-navigation-prev"
       v-on:click.prevent="triggerPageAdvance('backward')"
       v-bind:class="{ 'VueCarousel-navigation--disabled': !canAdvanceBackward }"
-      v-bind:style="`padding: ${clickTargetSize}px;
-                     margin-right: -${clickTargetSize}px;
-                     color: ${labelColor};
-                     font-size: ${labelSize}pt;`"
+      v-bind:style="{ padding: `${clickTargetSize}px`, 'margin-right': `-${clickTargetSize}px`,
+                     color: `${labelColor}`, 'font-size': `${labelSize}pt` }"
       v-html="prevLabel"></a>
     <a href="#"
       class="VueCarousel-navigation-button VueCarousel-navigation-next"
       v-on:click.prevent="triggerPageAdvance()"
       v-bind:class="{ 'VueCarousel-navigation--disabled': !canAdvanceForward }"
-      v-bind:style="`padding: ${clickTargetSize}px;
-                     margin-left: -${clickTargetSize}px;
-                     color: ${labelColor};
-                     font-size: ${labelSize}pt;`"
+      v-bind:style="{ padding: `${clickTargetSize}px`, 'margin-left': `-${clickTargetSize}px`,
+                     color: `${labelColor}`, 'font-size': `${labelSize}pt` }"
       v-html="nextLabel"></a>
   </div>
 </template>
